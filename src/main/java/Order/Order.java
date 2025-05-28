d6165c503c1167888f2902f3b4807b8de6de7007
Elle-Dorvil <rachelle.dorvil@yahoo.com>
2025-05-28 02:02:16 -0400

    Added code to my order class

--- src/main/java/Order/Order.java
+++ src/main/java/Order/Order.java
@@ -1,4 +1,31 @@
 package Order;
 
+import Sandwich.Sandwich;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private List<Sandwich> sandwiches = new ArrayList<>();
+    private List<Drink> drinks = new ArrayList<>();
+    private List<Chips> chips = new ArrayList<>();
+
+    public void addSandwich(Sandwich sandwich) {
+        sandwiches.add(sandwich);
+    }
+    public void addDrink(Chips chip) {
+        chips.add(chip);
+    }
+    public void displayOrderDetails() {
+        System.out.println("Order Details: ");
+        for (Sandwich s : sandwiches) {
+            s.displaySandwich();
+        }
+        for (Drink d : drinks) {
+            d.displayDrink();
+        }
+        for (Chips c : chips) {
+            c.displayChips();
+        }
+    }
 }
