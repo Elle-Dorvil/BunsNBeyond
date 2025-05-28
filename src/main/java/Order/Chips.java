f5a045c992d7bfe86034ebc5fdc01c271dfaaee6
Elle-Dorvil <rachelle.dorvil@yahoo.com>
2025-05-28 01:47:57 -0400

    Created Chips class

--- src/main/java/Order/Chips.java
+++ src/main/java/Order/Chips.java
@@ -1,4 +1,12 @@
 package Order;
 
 public class Chips {
+    private String flavor;
+
+    public Chips(String flavor) {
+        this.flavor = flavor;
+    }
+    public void displayChips() {
+        System.out.println("Chips: " + flavor);
+    }
 }
