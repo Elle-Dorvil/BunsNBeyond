c38ba31f4c4256035a7be734b2bf3ddd61568bda
Elle-Dorvil <rachelle.dorvil@yahoo.com>
2025-05-28 01:43:48 -0400

    Created Drink abstracts

--- src/main/java/Order/Drink.java
+++ src/main/java/Order/Drink.java
@@ -1,4 +1,14 @@
 package Order;
 
 public class Drink {
+    private String flavor;
+    private int size;
+
+    public Drink(String flavor, int size) {
+        this.flavor = flavor;
+        this.size = size;
+    }
+    public void displayDrink() {
+        System.out.println("Drink: " + flavor + "(" + size + ")");
+    }
 }
