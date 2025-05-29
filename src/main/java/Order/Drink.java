3c7c1391324f42170c311eff9acf976ffdbd025c
Elle-Dorvil <rachelle.dorvil@yahoo.com>
2025-05-29 11:13:52 -0400

    created getPrice and DisplayDrink in drink class

--- src/main/java/Order/Drink.java
+++ src/main/java/Order/Drink.java
@@ -2,13 +2,22 @@ package Order;
 
 public class Drink {
     private String flavor;
-    private int size;
+    private String size;
 
-    public Drink(String flavor, int size) {
+    public Drink(String flavor, String size) {
         this.flavor = flavor;
-        this.size = size;
+        this.size = size.toLowerCase();
+    }
+
+    public double getPrice() {
+        return switch (size) {
+            case "small" -> 2.00;
+            case "medium" -> 2.50;
+            case "large" -> 3.00;
+            default -> 0.0;
+        };
     }
     public void displayDrink() {
-        System.out.println("Drink: " + flavor + "(" + size + ")");
+        System.out.printf("Drink: %-10s (%s) $%.2f\n ", flavor, size, getPrice());
     }
 }
